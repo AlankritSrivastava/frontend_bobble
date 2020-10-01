a3bdb05e131f1647b105c2b026663e9dfe319937
AlankritSrivastava <alankritsri2000@gmail.com>
2020-10-01 23:36:25 +0530

    Updates

---
+++ precache-manifest.2bc24b471a3f3d911d8392a3700ead18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c50a7a0e1464681b778ab320d88c243e",
+    "url": "/frontend_bobble/index.html"
+  },
+  {
+    "revision": "4e0e31d76abed0244ddf",
+    "url": "/frontend_bobble/static/css/main.80919131.chunk.css"
+  },
+  {
+    "revision": "4ef557c9c185df8d71cb",
+    "url": "/frontend_bobble/static/js/2.a70d5574.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/frontend_bobble/static/js/2.a70d5574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e0e31d76abed0244ddf",
+    "url": "/frontend_bobble/static/js/main.29c048d6.chunk.js"
+  },
+  {
+    "revision": "5781affaf92e296fb987",
+    "url": "/frontend_bobble/static/js/runtime-main.ac2bcc90.js"
+  },
+  {
+    "revision": "389874667d69fa8397e67ebafcc05178",
+    "url": "/frontend_bobble/static/media/bobble.38987466.jpg"
+  }
+]);
